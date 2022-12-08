4c3f4935c028c16bf3a1cc3e684ce2eab2e88955
Pierre Fournier <pierre00fournier@gmail.com>
2022-12-08 15:45:02 +0100

    pushing everywhere

--- train.sh
+++ train.sh
@@ -2,14 +2,12 @@
 
 "${HOME}"/dl_toolbox/venv/bin/python \
 "${HOME}"/dl_toolbox/dl_toolbox/train/train.py \
---data_path /d/pfournie/ai4geo/data/miniworld_tif \
---splitfile_path "${HOME}"/ai4geo/splits/mw/christchurch.csv \
---datamodule Splitfile \
---labels base \
---crop_size 256 \
+--data_path /data/NWPU-RESISC45 \
+--datamodule FromFolderDataset \
+--folder_dataset Resisc \
 --img_aug d4 \
---test_folds 0 \
---train_folds 5 6 7 8 \
+--test_idxs 0 \
+--train_idxs 5 6 7 8 \
 --batch_size 4 \
 --workers 6 \
 --max_epochs 10 \
@@ -36,6 +34,11 @@
 --limit_train_batches 1 \
 --limit_val_batches 1 \
 --gpu 1
+
+--splitfile_path "${HOME}"/ai4geo/splits/mw/christchurch.csv \
+--datamodule Splitfile \
+--labels base \
+--crop_size 256 \
 
 #--ignore_index -1 \
 
