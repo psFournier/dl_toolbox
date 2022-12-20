0402c8a933548a67f2a8b9608852a4d4357fb2e7
Pierre Fournier <pierre00fournier@gmail.com>
2022-12-20 14:34:38 +0100

    d4 ok

--- dl_toolbox/augmentations/d4.py
+++ dl_toolbox/augmentations/d4.py
@@ -13,7 +13,7 @@ class Vflip(torch.nn.Module):
 
         if torch.rand(1).item() < self.p:
             img = F.vflip(img)
-            if label and label.dim>2:
+            if label is not None and label.dim()>2:
                 label = F.vflip(label)
             return img, label
 
@@ -29,7 +29,7 @@ class Hflip:
 
         if torch.rand(1).item() < self.p:
             img = F.hflip(img)
-            if label and label.dim>2:
+            if label is not None and label.dim()>2:
                 label = F.hflip(label)
             return img, label
 
@@ -45,7 +45,8 @@ class Transpose1(torch.nn.Module):
 
         if torch.rand(1).item() < self.p:
             img = torch.transpose(img, -2, -1)
-            if label and label.dim>2: label = torch.transpose(label, -2, -1)
+            if label is not None and label.dim()>2: 
+                label = torch.transpose(label, -2, -1)
             return img, label
 
         return img, label
@@ -62,7 +63,7 @@ class Transpose2(torch.nn.Module):
             img = img.flip(-1)
             img = img.flip(-2)
             img = torch.transpose(img, -2, -1)
-            if label and label.dim>2:
+            if label is not None and label.dim()>2:
                 label = label.flip(-1)
                 label = label.flip(-2)
                 label = torch.transpose(label, -2, -1)
@@ -80,7 +81,7 @@ class Rot90:
 
         if torch.rand(1).item() < self.p:
             img = torch.transpose(F.hflip(img), -2, -1)
-            if label and label.dim>2:
+            if label is not None and label.dim()>2:
                 label = torch.transpose(F.hflip(label), -2, -1)
             return img, label
 
@@ -97,7 +98,7 @@ class Rot180:
 
         if torch.rand(1).item() < self.p:
             img = F.vflip(F.hflip(img))
-            if label and label.dim>2:
+            if label is not None and label.dim()>2:
                 label = F.vflip(F.hflip(label))
             return img, label
 
@@ -114,28 +115,11 @@ class Rot270:
 
         if torch.rand(1).item() < self.p:
             img = torch.transpose(F.vflip(img), -2, -1)
-            if label and label.dim>2:
+            if label is not None and label.dim()>2:
                 label = torch.transpose(F.vflip(label), -2, -1)
             return img, label
 
         return img, label
-
-# class Rotate(torch.nn.Module):
-#
-#     def __init__(self, p=0.5, angles=(90, 180, 270)):
-#         super().__init__()
-#         self.p = p
-#         self.angles = angles
-#
-#     def __call__(self, img, label=None):
-#
-#         if torch.rand(1).item() < self.p:
-#             angle = random.choice(self.angles)
-#             img = F.rotate(img, angle)
-#             if label is not None: label = F.rotate(label, angle)
-#             return img, label
-#
-#         return img, label
 
 class D4:
 
@@ -163,26 +147,5 @@ class D4:
             ]
         )
 
-    def __call__(self, img, label):
+    def __call__(self, img, label=None):
         return self.d4(img, label)
-
-#class D4(torch.nn.Module):
-#
-#    def __init__(self, p=1):
-#        super().__init__()
-#        self.transforms = [
-#            NoOp(),
-#            Hflip(p=1),
-#            Vflip(p=1),
-#            Transpose1(p=1),
-#            Transpose2(p=1),
-#            Rot90(p=1),
-#            Rot180(p=1),
-#            Rot270(p=1)
-#        ]
-#
-#    def __call__(self, img, label):
-#
-#        t = random.choice(self.transforms)
-#
-#        return t(img, label)
