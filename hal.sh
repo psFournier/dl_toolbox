382f43c48b0dfb444b5049cee402077817120269
Pierre Fournier <pierre.fournier@onera.fr>
2023-01-05 14:20:17 +0000

    test git exclude train

--- hal.sh
+++ hal.sh
@@ -9,7 +9,8 @@ cd "${TMPDIR}"
 #cp -r "${USR_DIR}"/miniworld_tif/christchurch .
 #cp -r "${USR_DIR}"/SemCity-Toulouse-bench .
 #cp -r "${USRDIR}"/digitanie .
-cp -r /work/OT/ai4geo/DATA/DATASETS/DIGITANIE .
+#cp -r /work/OT/ai4geo/DATA/DATASETS/DIGITANIE .
+cp -r /work/OT/ai4usr/fournip/DIGITANIE .
 
 "${HOME}"/dl_toolbox/venv/bin/python "${HOME}"/dl_toolbox/dl_toolbox/train/train.py
 
