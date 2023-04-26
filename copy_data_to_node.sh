7aec7b5c455a0d7f71c26e5d2fb988869e4236ce
Pierre Fournier <pierre.fournier@onera.fr>
2023-04-26 09:21:28 +0000

    run on qdev on hal

---
+++ copy_data_to_node.sh
@@ -0,0 +1,8 @@
+cd "${TMPDIR}"
+mkdir DIGITANIE
+cities=( "Arcachon" "Biarritz" "Montpellier" "Toulouse" "Nantes" "Strasbourg" "Paris" )
+#cities=( "Toulouse" )
+for city in "${cities[@]}" ; do
+    rsync -rv --include="${city}/" --include="COS9/" --include="*.tif" --exclude="*" /work/OT/ai4geo/DATA/DATASETS/DIGITANIE/ DIGITANIE/
+    #rsync -rvL --include="${city}/" --include="COS9/" --include="*.tif" --exclude="*" /work/OT/ai4geo/DATA/DATASETS/DIGITANIE/ DIGITANIE/
+done
--- hal.sh
+++ hal.sh
@@ -3,17 +3,8 @@
 #PBS -l select=1:ncpus=6:mem=92G:ngpus=1
 #PBS -l walltime=12:00:00
 
+bash "${HOME}"/dl_toolbox/copy_data_to_node.sh
+
 module load python
-
-cd "${TMPDIR}"
-mkdir DIGITANIE
-#cities=( "Arcachon" "Biarritz" "Montpellier" "Toulouse" "Nantes" "Strasbourg" "Paris" )
-cities=( "Toulouse" )
-for city in "${cities[@]}" ; do
-    rsync -rv --include="${city}/" --include="COS9/" --include="*.tif" --exclude="*" /work/OT/ai4geo/DATA/DATASETS/DIGITANIE/ DIGITANIE/
-    #rsync -rvL --include="${city}/" --include="COS9/" --include="*.tif" --exclude="*" /work/OT/ai4geo/DATA/DATASETS/DIGITANIE/ DIGITANIE/
-done
-
 /work/OT/ai4usr/fournip/latest/bin/python "${HOME}"/dl_toolbox/dl_toolbox/train/train_from_splitfile.py
-
 module unload python
