7d94a0eebddc099ddd209eca1cd058eb26007908
Pierre Fournier <pierre.fournier@onera.fr>
2023-06-27 07:45:51 +0000

    hal script

--- hal.sh
+++ hal.sh
@@ -3,8 +3,8 @@
 #PBS -l select=1:ncpus=6:mem=92G:ngpus=1
 #PBS -l walltime=12:00:00
 
-bash "${HOME}"/dl_toolbox/copy_semcity_to_node.sh
+#bash "${HOME}"/dl_toolbox/copy_semcity_to_node.sh
 
 module load python
-/work/OT/ai4usr/fournip/latest/bin/python "${HOME}"/dl_toolbox/scripts/train_segment_semcity_cps.py
+/work/OT/ai4usr/fournip/latest/bin/python "${HOME}"/dl_toolbox/dl_toolbox/train.py paths=hal trainer=gpu
 module unload python
