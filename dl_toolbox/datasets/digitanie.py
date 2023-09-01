1e789d531ae7c26d4549be11a51ec97697f4737e
Pierre Fournier <pierre.fournier@onera.fr>
2023-09-01 09:29:01 +0000

    dataset for pneo toulouse

--- dl_toolbox/datasets/digitanie.py
+++ dl_toolbox/datasets/digitanie.py
@@ -2,10 +2,15 @@ import numpy as np
 import rasterio
 import torch
 import enum
+import random
 from torch.utils.data import Dataset
 
 from dl_toolbox.utils import label, get_tiles, merge_labels
-from rasterio.windows import Window
+import rasterio.windows as W
+from fiona.transform import transform as f_transform
+import rasterio.warp
+from shapely.geometry import box
+from rasterio.enums import Resampling
 
 all9 = [
     label("nodata", (250, 250, 250), {0}),
@@ -79,17 +84,17 @@ class DigitanieDataset(Dataset):
             "label": None if label is None else label.squeeze()
         }
     
-class DigitanieToaDataset(Dataset):
+class DigitanieUnlabeledToaDataset(Dataset):
     classes = classes
     
     def __init__(
         self,
-        img,
+        toa,
         bands,
         windows,
         transforms
     ):
-        self.img = img
+        self.toa = toa
         self.bands = bands
         self.transforms = transforms
         self.windows = windows
@@ -99,13 +104,74 @@ class DigitanieToaDataset(Dataset):
 
     def __getitem__(self, idx):
         window = self.windows[idx]
-        with rasterio.open(self.img, "r") as file:
+        with rasterio.open(self.toa, "r") as file:
             image = file.read(window=window, out_dtype=np.int16, indexes=self.bands)
         image = torch.clip(torch.from_numpy(image).float() / 8000., 0, 1)
         image, label = self.transforms(img=image, label=None)
         return {"image": image, "label": None}
     
+class DigitanieSublabeledToaDataset(Dataset):
+    classes = classes
     
+    def __init__(
+        self,
+        toa,
+        msks,
+        bands,
+        merge,
+        transforms
+    ):
+        self.toa = toa
+        self.msks = msks
+        self.bands = bands
+        self.transforms = transforms
+        self.class_list = self.classes[merge].value
+        self.merges = [list(l.values) for l in self.class_list]
+        self.crop_size = 512
+
+    def __len__(self):
+        return len(self.msks)
+
+    def __getitem__(self, idx):   
+        msk = rasterio.open(self.msks[idx])
+        toa = rasterio.open(self.toa)
+        
+        rows, cols = msk.shape
+        crop_size = self.crop_size * toa.res[0] / msk.res[0]
+        cx = random.randint(0, int(rows - crop_size))
+        cy = random.randint(0, int(cols - crop_size))
+        window = W.Window(cx, cy, crop_size, crop_size)
+        
+        label = msk.read(
+            window=window,
+            out_shape=(
+                msk.count,
+                self.crop_size,
+                self.crop_size
+            ),
+            resampling=Resampling.nearest,
+            out_dtype=np.uint8
+        )
+        
+        minx, miny, maxx, maxy = W.bounds(window, transform=msk.transform)
+        if msk.crs != toa.crs:
+            (minx, maxx), (miny, maxy) = f_transform(
+                msk.crs, toa.crs, [minx, maxx], [miny, maxy]
+            )
+        window = W.from_bounds(minx, miny, maxx, maxy, transform=toa.transform)
+        image = toa.read(window=window, out_dtype=np.int16, indexes=self.bands)
+        
+        image = torch.clip(torch.from_numpy(image).float() / 8000., 0, 1)
+        label = merge_labels(label, self.merges)
+        label = torch.from_numpy(label).long()
+        image, label = self.transforms(img=image, label=label)
+        msk.close()
+        toa.close()
+        return {
+            "image": image,
+            "label": label.squeeze()
+        }
+            
 #class Digitanie(Dataset):
 #    def __init__(
 #        self, data_src, bands, merge, crop_size, shuffle, transforms, crop_step=None
