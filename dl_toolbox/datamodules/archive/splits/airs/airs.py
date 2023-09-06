49555a9b6359d4f733409066678644f6d4de469b
Pierre Fournier <pierre.fournier@onera.fr>
2023-09-06 13:54:18 +0000

    digi dm

---
+++ dl_toolbox/datamodules/archive/splits/airs/airs.py
@@ -0,0 +1,71 @@
+import csv
+from pathlib import Path
+
+import rasterio
+
+
+with open(Path.home() / f"ai4geo/splits/airs/airs.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    path = Path("/data/AIRS/trainval")
+    writer.writerow(
+        [
+            "dataset_cls",
+            "tile_id",
+            "img_path",
+            "label_path",
+            "x0",
+            "y0",
+            "patch_width",
+            "patch_height",
+            "fold_id",
+        ]
+    )
+
+    i = 0
+    for full_image_path in (path / "val/image").iterdir():
+        full_label_path = path / "val/label" / full_image_path.name
+        with rasterio.open(full_image_path) as f:
+            height, width = f.shape
+            if width == 10000 and height == 10000:
+                m = f.dataset_mask().mean()
+                if m < 250:
+                    print(full_image_path)
+                else:
+                    writer.writerow(
+                        [
+                            "Airs",
+                            i,
+                            f"val/image/{full_image_path.name}",
+                            f"val/label/{full_image_path.name}",
+                            0,
+                            0,
+                            width,
+                            height,
+                            i % 5,
+                        ]
+                    )
+                    i += 1
+
+    for full_image_path in (path / "train/image").iterdir():
+        full_label_path = path / "train/label" / full_image_path.name
+        with rasterio.open(full_image_path) as f:
+            height, width = f.shape
+            if width == 10000 and height == 10000:
+                m = f.dataset_mask().mean()
+                if m < 250:
+                    print(full_image_path)
+                else:
+                    writer.writerow(
+                        [
+                            "Airs",
+                            i,
+                            f"train/image/{full_image_path.name}",
+                            f"train/label/{full_image_path.name}",
+                            0,
+                            0,
+                            width,
+                            height,
+                            i % 10 + 5,
+                        ]
+                    )
+                    i += 1
--- dl_toolbox/datamodules/flair/flair.py
+++ dl_toolbox/datamodules/flair/flair.py
@@ -6,7 +6,7 @@ from pytorch_lightning import LightningDataModule
 from pytorch_lightning.utilities import CombinedLoader
 from torch.utils.data import DataLoader, RandomSampler
 
-from dl_toolbox.datasets import DatasetFlair2
+import dl_toolbox.datasets as datasets
 from dl_toolbox.utils import CustomCollate
 
 from .utils import flair_gather_data
@@ -15,19 +15,16 @@ class Flair(LightningDataModule):
     
     train_domains = ["D004_2021", "D014_2020", "D029_2021", "D031_2019", "D058_2020", "D077_2021", "D067_2021", "D066_2021", "D033_2021", "D055_2018", "D072_2019", "D044_2020", "D017_2018", "D086_2020", "D049_2020", "D016_2020", "D063_2019", "D091_2021", "D070_2020", "D013_2020", "D023_2020", "D074_2020", "D021_2020", "D080_2021", "D078_2021", "D032_2019", "D081_2020", "D046_2019", "D052_2019", "D051_2019", "D038_2021", "D009_2019", "D034_2021", "D006_2020", "D008_2019", "D041_2021", "D035_2020", "D007_2020", "D060_2021", "D030_2021"]
     
-    test_domains = ["D012_2019", "D022_2021", "D026_2020", "D064_2021", "D068_2021", "D071_2020", "D075_2021", "D076_2019", "D083_2020", "D085_2019"    ]
+    test_domains = ["D012_2019", "D022_2021", "D026_2020", "D064_2021", "D068_2021", "D071_2020", "D075_2021", "D076_2019", "D083_2020", "D085_2019"]
     
-class DatamoduleFlair1(Flair):
-
     def __init__(
         self,
         data_path,
         merge,
-        prop,
+        labeled_prop,
+        unlabeled_prop,
         bands,
-        train_tf,
-        val_tf,
-        test_tf,
+        dataset_tf,
         batch_size,
         num_workers,
         pin_memory,
@@ -38,149 +35,21 @@ class DatamoduleFlair1(Flair):
         super().__init__()
         self.data_path = Path(data_path)
         self.merge = merge
-        self.prop = prop
+        self.labeled_prop = labeled_prop
+        self.unlabeled_prop = unlabeled_prop
         self.bands = bands
-        self.train_tf = train_tf
-        self.val_tf = val_tf
-        self.test_tf = test_tf
+        self.dataset_tf = dataset_tf
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.pin_memory = pin_memory
         self.in_channels = len(self.bands)
-        self.classes = DatasetFlair2.classes[merge].value
+        self.classes = datasets.Flair.classes[merge].value
         self.num_classes = len(self.classes)
         self.class_names = [l.name for l in self.classes]
         self.class_colors = [(i, l.color) for i, l in enumerate(self.classes)]
         self.class_weights = (
             [1.0] * self.num_classes if class_weights is None else class_weights
         )
-    
-    def prepare_data(self):
-        domains = [self.data_path / "FLAIR_1" / "train" / d for d in self.train_domains]
-        random.shuffle(domains)
-        train_domains, val_domains, test_domains = [], [], []
-        for i, domain in enumerate(domains):
-            if i%100 < int(self.prop * 40 / 100):
-                train_domains.append(domain)
-            elif i%100 >= int(90 * 40 / 100):
-                val_domains.append(domain)
-            #else:
-            #    test_domains.append(domain)
-        def get_data_dict(domains):
-            img, msk, mtd = flair_gather_data(
-                domains, path_metadata=None, use_metadata=False, test_set=False
-            )
-            return {"IMG":img, "MSK":msk}
-        self.dict_train = get_data_dict(train_domains)
-        self.dict_val = get_data_dict(val_domains)
-        #self.dict_test = get_data_dict(test_domains)
-
-    def setup(self, stage):
-        if stage in ("fit", "validate"):
-            self.train_set = DatasetFlair2(
-                self.dict_train["IMG"],
-                self.dict_train["MSK"],
-                self.bands,
-                self.merge,
-                transforms=self.train_tf,
-            )
-
-            self.val_set = DatasetFlair2(
-                self.dict_val["IMG"],
-                self.dict_val["MSK"],
-                self.bands,
-                self.merge,
-                transforms=self.val_tf,
-            )
-        if stage in ("test", "predict"):
-            dataset = DatasetFlair2(
-                self.dict_test["IMG"],
-                self.dict_test["MSK"],
-                self.bands,
-                self.merge,
-                transforms=self.test_tf,
-            )
-            if stage == "test":
-                self.test_set = dataset
-            else:
-                self.pred_set = dataset
-                
-    def get_loader(self, dataset):
-        return partial(
-            DataLoader,
-            dataset=dataset,
-            collate_fn=CustomCollate(),
-            batch_size=self.batch_size,
-            num_workers=self.num_workers,
-            pin_memory=self.pin_memory
-        )
-                       
-    def train_dataloader(self):
-        train_dataloaders = {}
-        train_dataloaders["sup"] = self.get_loader(self.train_set)(
-            shuffle=True,
-            drop_last=True,
-        )
-        return CombinedLoader(train_dataloaders, mode="min_size")
-    
-    def val_dataloader(self):
-        return self.get_loader(self.val_set)(
-            shuffle=False,
-            drop_last=False,
-        )
-
-    def predict_dataloader(self):
-        return self.get_loader(self.pred_set)(
-            shuffle=False,
-            drop_last=False,
-        )
-    
-    def test_dataloader(self):
-        return self.get_loader(self.test_set)(
-            shuffle=False,
-            drop_last=False,
-        )
-
-class DatamoduleFlair2(DatamoduleFlair1):
-    
-    def __init__(
-        self,
-        *args,
-        **kwargs
-    ):
-        super().__init__(*args, **kwargs)
-        #assert 0 < self.prop < 90
-        
-    def prepare_data(self):
-        domains = [self.data_path / "FLAIR_1" / "train" / d for d in self.train_domains]
-        all_img, all_msk, all_mtd = flair_gather_data(
-            domains, path_metadata=None, use_metadata=False, test_set=False
-        )
-        self.dict_train = {"IMG": [], "MSK": [], "MTD": []}
-        self.dict_val = {"IMG": [], "MSK": [], "MTD": []}
-        self.dict_test = {"IMG": [], "MSK": [], "MTD": []}
-        for i, (img, msk) in enumerate(zip(all_img, all_msk)):
-            if i%100 < self.prop:
-                self.dict_train["IMG"].append(img)
-                self.dict_train["MSK"].append(msk)
-            elif i%100 >= 90:
-                self.dict_val["IMG"].append(img)
-                self.dict_val["MSK"].append(msk)
-            else:
-                self.dict_test["IMG"].append(img)
-                self.dict_test["MSK"].append(msk)
-                
-class DatamoduleFlair2Semisup(DatamoduleFlair2):
-    
-    def __init__(
-        self,
-        unlabeled_prop,
-        pseudo_label_dir=None,
-        *args,
-        **kwargs
-    ):
-        super().__init__(*args, **kwargs)
-        self.unlabeled_prop = unlabeled_prop
         
     def prepare_data(self):
         domains = [self.data_path / "FLAIR_1" / "train" / d for d in self.train_domains]
@@ -190,40 +59,70 @@ class DatamoduleFlair2Semisup(DatamoduleFlair2):
         self.dict_train = {"IMG": [], "MSK": [], "MTD": []}
         self.dict_train_unlabeled = {"IMG": [], "MSK": [], "MTD": []}
         self.dict_val = {"IMG": [], "MSK": [], "MTD": []}
-        self.dict_test = {"IMG": [], "MSK": [], "MTD": []}
         for i, (img, msk) in enumerate(zip(all_img, all_msk)):
-            if self.prop <= i%100 <= self.prop + self.unlabeled_prop:
+            if self.labeled_prop <= i%100 < self.labeled_prop + self.unlabeled_prop:
                 self.dict_train_unlabeled["IMG"].append(img)
-            if i%100 < self.prop:
+            if i%100 < self.labeled_prop:
                 self.dict_train["IMG"].append(img)
                 self.dict_train["MSK"].append(msk)
-            elif i%100 >= 90:
+            elif 90 <= i%100:
                 self.dict_val["IMG"].append(img)
                 self.dict_val["MSK"].append(msk)
-            else:
-                self.dict_test["IMG"].append(img)
-                self.dict_test["MSK"].append(msk)
 
     def setup(self, stage):
-        super().setup(stage)
-        if stage in ("fit"):
-            self.unlabeled_set = DatasetFlair2(
-                self.dict_train_unlabeled["IMG"],
-                [],
+        if stage in ("fit", "validate"):
+            self.train_set = datasets.Flair(
+                self.dict_train["IMG"],
+                self.dict_train["MSK"],
                 self.bands,
                 self.merge,
-                #self.crop_size,
-                transforms=self.train_tf,
+                transforms=self.dataset_tf,
             )
-        
+            self.val_set = datasets.Flair(
+                self.dict_val["IMG"],
+                self.dict_val["MSK"],
+                self.bands,
+                self.merge,
+                transforms=self.dataset_tf,
+            )
+            if self.unlabeled_prop > 0:
+                self.unlabeled_set = datasets.Flair(
+                    self.dict_train_unlabeled["IMG"],
+                    [],
+                    self.bands,
+                    self.merge,
+                    transforms=self.dataset_tf,
+                )
+                       
     def train_dataloader(self):
         train_dataloaders = {}
-        train_dataloaders["sup"] = self.get_loader(self.train_set)(
-            shuffle=True,
-            drop_last=True,
-        )
-        train_dataloaders["unsup"] = self.get_loader(self.unlabeled_set)(
+        train_dataloaders["sup"] = DataLoader(
+            dataset=self.train_set,
+            collate_fn=CustomCollate(),
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
             shuffle=True,
             drop_last=True
         )
+        if self.unlabeled_prop > 0:
+            train_dataloaders["unsup"] = DataLoader(
+                dataset=self.unlabeled_set,
+                collate_fn=CustomCollate(),
+                batch_size=self.batch_size,
+                num_workers=self.num_workers,
+                pin_memory=self.pin_memory,
+                shuffle=True,
+                drop_last=True
+            )
         return CombinedLoader(train_dataloaders, mode="max_size_cycle")
+    
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=self.val_set,
+            collate_fn=CustomCollate(),
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+            drop_last=False,
+        )
