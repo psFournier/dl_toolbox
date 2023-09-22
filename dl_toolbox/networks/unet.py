ca780a638c290d0c29c130c9f8c9b4693fdede61
Pierre Fournier <pierre00fournier@gmail.com>
2023-09-22 22:09:58 +0200

    preoper nnUnet archi

--- dl_toolbox/networks/unet.py
+++ dl_toolbox/networks/unet.py
@@ -94,28 +94,32 @@ class Unet(nn.Module):
         self.out_channels = num_classes
         self.bilinear = bilinear
         self.max_pool = max_pool
-
-        self.inc = DoubleConv(in_channels, 64)
+        factor = 2 if bilinear else 1
+        
+        self.inc = DoubleConv(in_channels, 32)
+        self.down0 = Down(32, 64, max_pool)
         self.down1 = Down(64, 128, max_pool)
         self.down2 = Down(128, 256, max_pool)
-        self.down3 = Down(256, 512, max_pool)
-        factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor, max_pool)
-        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.down3 = Down(256, 512 // factor, max_pool)
+        #self.down4 = Down(512, 1024 // factor, max_pool)
+        #self.up1 = Up(1024, 512 // factor, bilinear)
         self.up2 = Up(512, 256 // factor, bilinear)
         self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, num_classes)
+        self.up4 = Up(128, 64 // factor, bilinear)
+        self.up5 = Up(64, 32, bilinear)
+        self.outc = OutConv(32, num_classes)
 
     def forward(self, x):
-        x1 = self.inc(x)
+        x0 = self.inc(x)
+        x1 = self.down0(x0)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
+        #x5 = self.down4(x4)
+        #x = self.up1(x5, x4)
+        x = self.up2(x4, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
+        x = self.up5(x, x0)
         logits = self.outc(x)
         return logits
