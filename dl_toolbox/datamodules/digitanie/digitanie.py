9f3ea044abc5706a2382fc67903fb9a3e5c377ab
Pierre Fournier <psfournier@mailbox.org>
2023-10-24 18:08:40 +0200

    digitanie crop 256

--- dl_toolbox/datamodules/digitanie/digitanie.py
+++ dl_toolbox/datamodules/digitanie/digitanie.py
@@ -21,7 +21,7 @@ import dl_toolbox.datasets as datasets
 from dl_toolbox.utils import CustomCollate, get_tiles
 from .digi_polygons import digitanie_polygons
 from .digitanie_ai4geo import DigitanieAi4geo
-from dl_toolbox.transforms import NoOp
+from dl_toolbox.transforms import NoOp, RandomCrop2
 
 
 def is_window_in_poly(w, tf, p):
@@ -69,7 +69,7 @@ class Digitanie(DigitanieAi4geo):
             self.train_u_set = datasets.DigitanieUnlabeledToa(
                 toa=self.toa,
                 bands=[1,2,3],
-                transforms=self.get_tf(NoOp(), self.city),
+                transforms=self.get_tf(RandomCrop2(256), self.city),
                 windows=self.toa_windows[::self.unsup]
             )
         self.val_set = datasets.Digitanie(
