12d4fe7f7a303752bb0904125d7ba736ec82e643
Pierre Fournier <pierre00fournier@gmail.com>
2025-01-31 17:43:36 +0100

    datamodule xview runs

--- src/dl_toolbox/datamodules/xview.py
+++ src/dl_toolbox/datamodules/xview.py
@@ -5,6 +5,7 @@ from pycocotools.coco import COCO
 
 from dl_toolbox.datasets import xView1
 from dl_toolbox.datamodules import Base
+from dl_toolbox.utils import get_tiles
 
 
 class xView(Base):
@@ -17,18 +18,26 @@ class xView(Base):
         super().__init__(*args, **kwargs)
         self.class_list = xView1.classes[self.merge].value   
         
-    def prepare_data(self):
+    def prepare_data(self):        
         coco = COCO(self.data_path/'XVIEW1'/'xView_train.json')
         ids = []
         merges = [list(l.values) for l in self.class_list]
         for merge in merges:
             # It seems that xview annotations geojson contain bboxes for images not in train_images nor val (test set not avail?)
-            merge_ids = [id for id in coco.getImgIds(catIds=merge) if id in coco.imgs.keys()]
-            self.root/coco.loadImgs(id)[0]["file_name"]
-            ids += merge_ids
+            ids += [id for id in coco.getImgIds(catIds=merge) if id in coco.imgs.keys()]
         nb_imgs = len(ids)
-        val_start, test_start = int(0.7*nb_imgs), int(0.8*nb_imgs)
-        self.train_ids, self.val_ids = ids[:val_start], ids[val_start:test_start]
+        val_start, test_start = int(0.7*nb_imgs), int(0.75*nb_imgs)
+        train_ids, val_ids = ids[:val_start], ids[val_start:test_start]
+        self.train_ids_and_windows = []
+        for id in train_ids:
+            img = coco.imgs[id]
+            w, h = img['width'], img['height']
+            self.train_ids_and_windows += [(id, w) for w in get_tiles(w,h,448,step_w=256)]
+        self.val_ids_and_windows = []
+        for id in val_ids:
+            img = coco.imgs[id]
+            w, h = img['width'], img['height']
+            self.val_ids_and_windows += [(id, w) for w in get_tiles(w,h,448,step_w=448)]
         self.coco = coco
     
     def setup(self, stage=None):
@@ -36,13 +45,13 @@ class xView(Base):
             merge=self.merge,
             coco_dataset=self.coco,
             root=self.data_path/'XVIEW1'/'train_images',
-            ids=self.train_ids
+            ids=self.train_ids_and_windows
         )
         self.val_set = xView1(
             merge=self.merge,
             coco_dataset=self.coco,
             root=self.data_path/'XVIEW1'/'train_images',
-            ids=self.val_ids
+            ids=self.val_ids_and_windows[::5]
         )
         self.train_set.init_tf(self.train_tf)
         self.val_set.init_tf(self.test_tf)
--- src/dl_toolbox/datasets/xview1/xview1.py
+++ src/dl_toolbox/datasets/xview1/xview1.py
@@ -1,17 +1,16 @@
 from collections import defaultdict
 from torch.utils.data import Dataset
 from torchvision import tv_tensors
-from torchvision.transforms.v2 import functional as F
+import torchvision.transforms.v2 as T
 from pathlib import Path
 import torch
-import torchvision.transforms.v2 as v2
 import rasterio
 import numpy as np
 import enum
 
 from dl_toolbox.utils import label, merge_labels_boxes
-
-from rasterio.windows import Window
+from functools import reduce
+import rasterio.windows as win
 
 
 
@@ -98,7 +97,9 @@ classes = enum.Enum(
     },
 )
 
-class xView1(Dataset):
+
+
+class xView1(torch.utils.data.Dataset):
     """
     Requires the `COCO API to be installed <https://github.com/pdollar/coco/tree/master/PythonAPI>`_.
 
@@ -107,49 +108,74 @@ class xView1(Dataset):
         annFile (string): Path to json annotation file.
     """
     classes = classes
-
+    
     def __init__(self, root, coco_dataset, ids, transforms=None, merge='all'):
         self.root = Path(root)
         self.coco = coco_dataset
-        self.ids = ids
+        self.ids_and_windows = ids
+        self.transforms = transforms
+        self.class_list = self.classes[merge].value
         self.init_tf(transforms)
-        self.class_list = self.classes[merge].value   
         
     def init_tf(self, tf):
-        self.transforms = v2.ToDtype(
+        self.transforms = T.ToDtype(
             dtype={tv_tensors.Image: torch.float32, "others":None},
             scale=True
         )
         if tf:
-            self.transforms = v2.Compose([self.transforms, tf])
+            self.transforms = T.Compose([self.transforms, tf])
 
     def __getitem__(self, index):
-        id = self.ids[index]
+        id, (l,t,w,h) = self.ids_and_windows[index]
+        window = win.Window(l,t,w,h)
         path = self.root/self.coco.loadImgs(id)[0]["file_name"]
         with rasterio.open(path, "r") as file:
-            image = file.read(out_dtype=np.uint8)
+            image = file.read(window=window, out_dtype=np.uint8) #Window(col_off, row_off, width, height)
         tv_image = tv_tensors.Image(torch.from_numpy(image))
-                
-        target = self.coco.loadAnns(self.coco.getAnnIds(id))
-        target = list_of_dicts_to_dict_of_lists(target)
+        
+        anns = []
+        ann_ids = self.coco.getAnnIds(id)
+        for ann_id in ann_ids:
+            ann = dict(self.coco.anns[ann_id])
+            bb_win = win.Window(*ann['bbox'])
+            if win.intersect([window, bb_win]):
+                x,y,w,h = win.intersection([window, bb_win]).flatten()
+                x -= l
+                y -= t
+                ann['bbox'] = (x,y,w,h)
+                anns.append(ann)
+        
+        #target = self.coco.loadAnns(self.coco.getAnnIds(id))
+        target = list_of_dicts_to_dict_of_lists(anns)
+        labels = torch.tensor(target["category_id"]) if target else torch.empty(0,)
+        boxes = torch.as_tensor(target["bbox"]).float() if target else torch.empty(0,4).float()
+        #merged_labels, merged_boxes = merge_labels_boxes(labels, boxes, self.class_list)
+        
+        merged_labels = []
+        merged_boxes = []
+        for i, l in enumerate(self.class_list, 1): 
+            # indices of tgts whose label belongs to the i-th merge
+            idx = reduce(torch.logical_or, [labels == v for v in l.values])
+            # i+1 because in detection, class label 0 should be left for no-obj in algos
+            merged_labels.append(i * torch.ones_like(labels[idx]))
+            merged_boxes.append(boxes[idx])
+        merged_labels = torch.cat(merged_labels, dim=0)
+        merged_boxes = torch.cat(merged_boxes, dim=0)
+        
         tv_target = {}
-        
-        labels = torch.tensor(target["category_id"])
-        boxes = torch.as_tensor(target["bbox"]).float()
-        merged_labels, merged_boxes = merge_labels_boxes(labels, boxes, self.class_list)
-        
         tv_target["boxes"] = tv_tensors.BoundingBoxes(
             merged_boxes,
             format=tv_tensors.BoundingBoxFormat.XYWH,
-            canvas_size=tuple(F.get_size(tv_image)),
+            canvas_size=tuple(T.functional.get_size(tv_image)),
         )
         tv_target['labels'] = merged_labels.long()
+        
         if self.transforms is not None:
             tv_image, tv_target = self.transforms(tv_image, tv_target)
         return {'image': tv_image, 'target': tv_target, 'path': path}
 
     def __len__(self):
-        return len(self.ids)
+        return len(self.ids_and_windows)
     
     @classmethod
     def collate(cls, batch):
